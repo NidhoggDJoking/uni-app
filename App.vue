598cd8170c24d5585f9f1921f02999cd0ec23d0d
joking <250348426@qq.com>
2019-12-24 14:19:16 +0800

    应用生命周期

--- App.vue
+++ App.vue
@@ -1,13 +1,18 @@
 <script>
 	export default {
+		// uni-app 支持如下应用生命周期函数：
+		
+		// 当uni-app 初始化完成时触发（全局只触发一次）
 		onLaunch: function() {
-			console.log('App Launch')
+			console.log('App Launch:' + '当 uni-app 初始化完成时触发（全局只触发一次）')
 		},
+		// 当 uni-app 启动，或从后台进入前台显示
 		onShow: function() {
-			console.log('App Show')
+			console.log('App Show:' + '当 uni-app 启动，或从后台进入前台显示')
 		},
+		// 当 uni-app 从前台进入后台
 		onHide: function() {
-			console.log('App Hide')
+			console.log('App Hide:' + '当 uni-app 从前台进入后台')
 		}
 	}
 </script>
