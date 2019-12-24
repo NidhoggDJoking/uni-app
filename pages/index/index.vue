0ab6db4a96ff1dc25fa75c97185a73f39e65111b
joking <250348426@qq.com>
2019-12-24 18:11:37 +0800

    index.vue

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,16 +9,38 @@
 
 <script>
 	export default {
+		// 页面生命周期
+		onLoad() {
+			console.log('监听页面加载，其参数为上个页面传递的数据，参数类型为Object(用于页面传参)');
+		},
+		onShow(){
+			console.log('监听页面显示。页面每次出现在屏幕上都触发，包括从下级页面点返回露出当前页面');
+			this.getDate();
+		},
+		onReady(){
+			console.log('监听页面初次渲染完成。注意如果渲染速度快，会在页面进入动画完成前触发');
+		},
+		onHide(){
+			console.log('监听页面隐藏');
+		},
+		onUnload(){
+			console.log('监听页面卸载');
+		},
 		data() {
 			return {
 				title: 'Hello'
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			getDate(){
+				uni.request({
+				    url: 'http://yxx.h-etrip.com:9092/etrip/api/app/page/block?code=mobile.index.top-swiper',
+				    success: (res) => {
+				        console.log(res.data);
+				        this.title = 'request success';
+				    }
+				});
+			}
 		}
 	}
 </script>
